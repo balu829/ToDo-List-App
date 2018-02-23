1a746935809824dce6ee8aa291b53c7bc287f21c
balu829 <34113836+balu829@users.noreply.github.com>
2018-02-23 15:34:38 -0500

    Added function todoListener

--- javascript.js
+++ javascript.js
@@ -2,19 +2,59 @@
 (function (evt) {
         //Send AJAX call to get the  json data
         var newStickyRequest = new XMLHttpRequest();
-        newStickyRequest.open('GET', 'data.json', true);
+        newStickyRequest.open('GET', './JSON data/data.json', true);//path to json file
         newStickyRequest.onload = function (e) {
             if (this.readyState == 4 && this.status == 200) {
-              toDoListener(this.responseText);
+              toDoListener(this.responseText);//invokes the function after receiving the response 
             }
         };
-        newStickyRequest.send();
+        newStickyRequest.send();//sends the request to server
     })();
 
 
 function toDoListener(responseText){
-  var json=JSON.parse(responseText);
-  var htm=document.getElementById("todoItems");
-  var div=document.getElementById("divToDoItems");
+	
+  var json=JSON.parse(responseText);//to parse json data from response text
+  var div=document.getElementById("divToDoItems");//get div element where we need to add data
+    for(var i=0; i<json.length; i++){
+    var details=document.createElement("details");//creating details element
+    details.setAttribute("class",'d')
+    var summary=document.createElement("summary");//creating summary element
+    var p=document.createElement("p");//creating p element
+    var summaryText=document.createTextNode(json[i].name); //creating text for summary
+    var pText=document.createTextNode("Item: " +json[i].description );//creating text for p
+    summary.appendChild(summaryText);//Appending summary text to summary
+    var span = document.createElement("SPAN");//creating span element
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt); //appending span text
+    p.appendChild(pText);
+    var br=document.createElement("br");
+    p.appendChild(br);
+    var br1=document.createElement("br");
+    p.appendChild(br1);
+    pText=document.createTextNode("Author: " + json[i].author);//appending json data to p element text
+    p.appendChild(pText);
+    var br=document.createElement("br");
+    p.appendChild(br);
+    var br1=document.createElement("br");
+    p.appendChild(br1);
+    var date=new Date();  //getting current date
+    pText=document.createTextNode("Date created: " + date.toDateString());//adding to p element
+    p.appendChild(pText);
+	//appending all elements to details
+    details.appendChild(span);
+    details.appendChild(summary);
+    details.appendChild(p);
+    div.appendChild(details);
+	
+	
+	    for (j = 0; j < close1.length; j++) {
+    close1[j].onclick= function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+      };
+    }
+}
 
 };
